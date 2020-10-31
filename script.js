5074a9632d703db14cf4e3d7da7d147fc6df3edd
1uca116 <l.bakirova116@gmail.com>
2020-10-31 22:52:22 +0300

    js is ready

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-
-
-
-
-
-
-
 function saveInfo() {
     let name = document.getElementsByClassName('popup__type-name')[0].value;
     let about = document.getElementsByClassName('popup__type-job')[0].value;
-    console.log(document.getElementsByClassName('profile__name')[0].innerHTML);
     document.getElementsByClassName('profile__name')[0].innerHTML = name;
     document.getElementsByClassName('profile__job')[0].textContent = about;
-    changePopUpVisibility();
+    closePopUp();
 }
 
-function changePopUpVisibility() {
+function showPopUp() {
     let popUp = document.getElementsByClassName('popup')[0];
-    if(popUp.classList.contains('popup_opened')) {
-        popUp.classList.remove('popup_opened');
-    } else {
+    if(!popUp.classList.contains('popup_opened')) {
         popUp.classList.add('popup_opened');
     }
 }
 
+function closePopUp() {
+    let popUp = document.getElementsByClassName('popup')[0];
+    if(popUp.classList.contains('popup_opened')) {
+        popUp.classList.remove('popup_opened');
+    }
+    clearInputs();
+}
 
+function clearInputs() {
+    document.getElementsByClassName('popup__type-name')[0].value = '';
+    document.getElementsByClassName('popup__type-job')[0].value = '';
+}
