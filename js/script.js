8c1d301c846c69beb411079a88786bb4348e96f3
1uca116 <l.bakirova116@gmail.com>
2020-11-01 21:39:18 +0300

    fixed fonts+form

--- js/script.js
+++ js/script.js
@@ -5,23 +5,24 @@ let popUpElement = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__edit")
 let popUpCloseButton = document.querySelector(".popup__button-close")
 
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+
+let popupInputName = formElement.querySelector(".popup__type-name");
+let popupInputJob = formElement.querySelector(".popup__type-job");
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let nameInput = formElement.querySelector(".popup__type-name");
-    let jobInput = formElement.querySelector(".popup__type-job");
-
-    let profileName = document.querySelector(".profile__name");
-    let profileJob = document.querySelector(".profile__job");
-
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
+    profileName.textContent = popupInputName.value;
+    profileJob.textContent = popupInputJob.value;
     closePopUp();
 }
 
 function openPopUp() {
     popUpElement.classList.add("popup_opened");
+    popupInputName.value = profileName.textContent;
+    popupInputJob.value = profileJob.textContent;
 }
 function closePopUp() {
     popUpElement.classList.remove("popup_opened");
