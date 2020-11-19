8e40a6bb67e284675b0dda9be7dc75a6c27070f4
1uca116 <l.bakirova116@gmail.com>
2020-11-19 22:37:24 +0300

    added popup picture

--- js/script.js
+++ js/script.js
@@ -1,9 +1,54 @@
 // Находим форму в DOM
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const userTemplate = document.querySelector("#card-template");
+const userTemplateContent = userTemplate.content;
+const placesList = document.querySelector(".places");
+
+initialCards.forEach(item => {
+    let newElement = userTemplateContent.cloneNode(true);
+    newElement.querySelector(".card__image").src = item.link;
+    newElement.querySelector(".card__name").textContent = item.name;
+
+    newElement.querySelector('.card__button-like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('card__button-like_active');
+    }); 
+    placesList.append(newElement);
+})
+
+
+
+
 let formElement = document.querySelector(".popup__form");
 let popUpElement = document.querySelector(".popup");
 
-let editButton = document.querySelector(".profile__edit")
-let popUpCloseButton = document.querySelector(".popup__button-close")
+let editButton = document.querySelector(".profile__edit");
+let popUpCloseButton = document.querySelector(".popup__button-close");
 
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
